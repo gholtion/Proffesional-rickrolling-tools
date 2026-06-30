f285e59bf7a4a2af63883ccd25e34b59bd53df42
gholtion <159619508+gholtion@users.noreply.github.com>
2026-06-30 15:16:19 +0300

    Update and rename dumdum.bat to simple batch/name this something cool.bat

--- simple batch/name this something cool.bat
+++ simple batch/name this something cool.bat
@@ -1,4 +1,4 @@
 title Never gonna give you up :-)
 echo x=msgbox("Ready for something cool?", 0, "Alert") >> msgbox.vbs
 cscript //nologo msgbox.vbs
-start /MAX "Hahah get rickrolled" "C:\Users\Tratengi\commad\MagnumOpus.mp4"
+start /MAX "Hahah get rickrolled" "MagnumOpus.mp4"

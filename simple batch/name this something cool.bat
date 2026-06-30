23ff82bed02b32c900989ab835198597b8e25bb2
gholtion <159619508+gholtion@users.noreply.github.com>
2026-06-30 15:20:04 +0300

    Update name this something cool.bat

--- simple batch/name this something cool.bat
+++ simple batch/name this something cool.bat
@@ -1,4 +1,4 @@
 title Never gonna give you up :-)
-echo x=msgbox("Ready for something cool?", 0, "Alert") >> msgbox.vbs
 cscript //nologo msgbox.vbs
+timeout /t 1
 start /MAX "Hahah get rickrolled" "MagnumOpus.mp4"
